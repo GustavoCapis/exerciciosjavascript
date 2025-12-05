de3ec0c57603dac8e4ddc3282f0deed6f8d2e9f5
Gustavo Nepomuceno Capistrano <gustavo.capy@gmail.com>
2025-12-05 18:27:46 -0300

    feat: add exemple on how to add new object properties.

--- objetos/propriedades.js
+++ objetos/propriedades.js
@@ -1,5 +1,23 @@
 //Como adicionar, editar e remover propriedades dos objetos
 
+//Adicionando propriedades à objetos
+const person = {
+    name: "Gustavo",
+};
+
+//Existem algumas formas:
+person.age = 24;
+console.log(person);
+
+person["hair"] = "curly";
+console.log(person);
+//Os dois acima são úteis quando você sabe o nome da propriedade que quer colocar, mas se não for o caso:
+//Key representa um dado dinâmico de uma API por exemplo
+const key = "weight";
+person[key] = 60;
+console.log(person);
+
+
 //Removendo propriedades de objetos
 const objeto = {
     nome: "Gustavo",
@@ -11,3 +29,4 @@ console.log(objeto);
 delete objeto.idade
 //Após usar o delete
 console.log(objeto);
+
